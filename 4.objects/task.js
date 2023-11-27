52c5397d0b2ee4e6b5c9b9ebdf9b368abba3209e
Alsu Kirillova <sai-alsu@mail.ru>
2023-11-27 20:43:11 +0300

    Четвертое задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (Object.getOwnPropertyDescriptor(this, "marks")) {
+		this.marks.push(...marks);
+	}
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (Object.getOwnPropertyDescriptor(this, "marks") && this.marks.length) {
+		let acc = 0;
+		for (mark of this.marks) {
+			acc += mark;
+		}
+		return acc / this.marks.length;
+	}
+
+	return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
