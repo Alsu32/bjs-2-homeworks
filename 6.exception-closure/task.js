e4c7ca163b1e3374a2a23d12c748a43fcd2d7336
Alsu Kirillova <sai-alsu@mail.ru>
2024-01-04 17:25:29 +0300

    Задание №6 выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿function parseCount(value) {
+	let parseResult = Number.parseFloat(value);
+	if (isNaN(parseResult)) {
+		throw new Error("Невалидное значение");
+	} 
+	return parseResult;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;	
+	}
+}
+
+class Triangle {
+	constructor(num1, num2, num3) {
+		if (num1 + num2 < num3 || num2 + num3 < num1 || num1 + num3 < num2) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		} else {
+			this.num1 = num1;
+			this.num2 = num2;
+			this.num3 = num3;
+		}
+	} 
+
+	get perimeter() {
+		return +(this.num1 + this.num2 +this.num3).toFixed(3);
+	}
+
+	get area() {
+		let halfPerimeter = this.perimeter / 2;
+		return +Math.sqrt(halfPerimeter * (halfPerimeter - this.num1) * (halfPerimeter - this.num2) * (halfPerimeter - this.num3)).toFixed(3);
+	}
+
+}
+
+function getTriangle(num1, num2, num3) {
+	let triangle;
+	try {
+		triangle = new Triangle(num1, num2, num3);
+	} catch (error) {
+		triangle = {
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			},		
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			}
+		};
+	} finally {
+		return triangle;
+	}
+}
